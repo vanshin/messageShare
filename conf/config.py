e1d611fccd6a4a47ff0134ecbd2394ec15110ff9
vanshin <kfx721@hotmail.com>
2018-06-10 18:50:01 +0800

    edit

---
+++ conf/config.py
@@ -0,0 +1,11 @@
+#coding=utf8
+
+dbconf = 'mysql+pymysql://root:vanshin1111@127.0.0.1:3306/se_message?charset=utf8'
+
+USER_API = {
+    'address': '192.168.0.3',
+    'port': '5002'
+}
+
+LOG_FILE = '/home/vanshin/log/mss_core.log'
+
