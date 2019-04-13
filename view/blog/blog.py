fabe5f96760c18903050e5038479b5d02a0a311e
vanshin <kfx721@hotmail.com>
2019-04-13 20:49:26 +0800

    edit

--- view/blog/blog.py
+++ view/blog/blog.py
@@ -1,6 +1,9 @@
 
 from base.handler import BaseView
 
+from model.article import article
+from model.article import db_blog
+
 class Article(BaseView):
 
     async def get(self):
@@ -12,6 +15,12 @@ class Article(BaseView):
         v = await self.build_args()
 
         return 'test'
+
+    async def post(self):
+        await db_blog.connect()
+        a = await article.create(id=125, title='test', body='kkkk', userid='123421')
+        return 'kk'
+
 
 
 
